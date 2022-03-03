b3f3267667f1cfd124d058ac6aa28c03eaaf0eee
Marcio Granzotto <oscensores@gmail.com>
2022-03-03 03:48:06 -0300

    Working

--- build_local_amd64.sh
+++ build_local_amd64.sh
@@ -1,1 +1,15 @@
-cd prusaslicer-novnc && docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/debian-base/amd64:5.2.3" --build-arg BUILD_ARCH=amd64 --progress plain --no-cache -t prusaslicer-novnc .
+cd prusaslicer-novnc && docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/debian-base/amd64:5.2.3" --build-arg BUILD_ARCH=amd64 --progress plain -t prusaslicer-novnc .
+# cd prusaslicer-novnc && \
+# docker run \
+#   --rm \
+#   -it \
+#   --name builder \
+#   --privileged \
+#   -v prusaslicer-novnc:/data \
+#   -v /var/run/docker.sock:/var/run/docker.sock:ro \
+#   homeassistant/amd64-builder \
+#   -t /data \
+#   --all \
+#   --test \
+#   -i prusaslicer-novnc-arm64 \
+#   -d local
--- prusaslicer-novnc/get_latest_prusaslicer_release.sh
+++ prusaslicer-novnc/get_latest_prusaslicer_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Get the latest release of PrusaSlicer for Linux (non-AppImage) using the GitHub API
+# Get the latest release of PrusaSlicer for Linux (non-tar.bz2) using the GitHub API
 # This was forked from https://github.com/dmagyar/prusaslicer-vnc-docker/blob/main/getLatestPrusaSlicerRelease.sh
 
 set -eu
@@ -13,22 +13,21 @@ if [[ $# -lt 1 ]]; then
   echo
   echo "	url_ver: Takes a parameter to specify the version to retrieve (note: some download urls have hex-encoded ascii characters)"
   echo "	url_ver example: $0 url_ver 2.0.0%2B"
-  echo "	output: https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.0.0/PrusaSlicer-2.0.0%2Blinux64-201905201652.AppImage"
+  echo "	output: https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.0.0/PrusaSlicer-2.0.0%2Blinux64-201905201652.tar.bz2"
   echo
   echo "	name_ver: Takes a parameter to specify the filename to retrieve (note: this has a '+' added on at the end of the provided version number)"
   echo "	name_ver example: $0 name_ver 2.0.0"
-  echo "	output: PrusaSlicer-2.0.0+linux64-201905201652.AppImage"
+  echo "	output: PrusaSlicer-2.0.0+linux64-201905201652.tar.bz2"
   echo
   exit 1
 fi
 
 baseDir="/slic3r"
-rm -rf $baseDir
 mkdir -p $baseDir
 
 if [[ ! -e "$baseDir/latestReleaseInfo.json" ]]; then
 
-  curl -SsL https://api.github.com/repos/prusa3d/PrusaSlicer/releases/57584879 > $baseDir/latestReleaseInfo.json
+  curl -SsL https://api.github.com/repos/prusa3d/PrusaSlicer/releases/latest > $baseDir/latestReleaseInfo.json
 
 fi
 
@@ -46,25 +45,22 @@ if [[ $# -gt 1 ]]; then
 
 fi
 
-target_arch=$(if [ "$SLICER_ARCH" == "amd64" ]; then echo "x64-GTK3"; else echo "armv7l-GTK3"; fi)
-
 if [[ "$1" == "url" ]]; then
 
-  echo "${releaseInfo}" | jq --arg arch "$target_arch" -r '.assets[] | .browser_download_url | select(test("PrusaSlicer-.+(-\\w)?.linux-" + $arch + ".+.AppImage"))'
+  echo "${releaseInfo}" | jq -r '.assets[] | .browser_download_url | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-GTK3.+.tar.bz2"))'
 
 elif [[ "$1" == "name" ]]; then
 
-  echo "${releaseInfo}" | jq --arg arch "$target_arch" -r '.assets[] | .name | select(test("PrusaSlicer-.+(-\\w)?.linux-" + $arch + ".+.AppImage"))'
+  echo "${releaseInfo}" | jq -r '.assets[] | .name | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-GTK3.+.tar.bz2"))'
 
 elif [[ "$1" == "url_ver" ]]; then
 
-  arch=$(if [ "$SLICER_ARCH" == "amd64" ]; then echo "64"; else echo "armv7l"; fi)
   # Note: Releases sometimes have hex-encoded ascii characters tacked on
   # So version '2.0.0+' might need to be requested as '2.0.0%2B' since GitHub returns that as the download URL
-  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .browser_download_url | select(test("PrusaSlicer-" + $VERSION + "linux64-.+.AppImage"))'
+  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .browser_download_url | select(test("PrusaSlicer-" + $VERSION + "linux64-.+.tar.bz2"))'
 
 elif [[ "$1" == "name_ver" ]]; then
 
-  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .name | select(test("PrusaSlicer-" + $VERSION + "\\+linux64-.+.AppImage"))'
+  echo "${allReleases}" | jq --arg VERSION "$VER" -r '.[] | .assets[] | .name | select(test("PrusaSlicer-" + $VERSION + "\\+linux64-.+.tar.bz2"))'
 
 fi
